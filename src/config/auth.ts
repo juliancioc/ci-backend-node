13eaff2d563bffcd6225bd87a5aa609a6911ef32
Juliancio Carvalho <juliancio_alves@hotmail.com>
2020-08-02 00:03:20 -0300

    create database

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    jwt: {
+        secret: '1f6096882c365ed1a78671a933f14546',
+        expiresIn: '1d'
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,31 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
+import routes from './routes';
+import AppError from './errors/AppError';
+
+import './database';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World' })
-});
+app.use(express.json());
+app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if(err instanceof AppError) {
+        return response.status(err.statusCode).json({
+            status: 'error',
+            message: err.message
+        });
+    }
+
+    console.error(err);
+
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal server error'
+    })
+})
 
 app.listen(3333, () => {
     console.log('😎️ Server started on port 3333!')
